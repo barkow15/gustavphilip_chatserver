a8a68ba8fbcddf399695b2eda43b1273f26383ae
Barkow <pbarkow@deloitte.dk>
2019-12-08 18:43:06 +0100

    Final edits and testing

--- src/KlientStart.java
+++ src/KlientStart.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class KlientExec {
     public static void main(String[] args) {
-        Klient klient = new Klient("localhost", 2000);
+        Klient klient = new Klient("localhost", 8888);
         // Start forbind metoden
         klient.forbind();
     }
